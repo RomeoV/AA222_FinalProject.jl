d59cbc6c5f00e9386bdf14b4f48384f6e0f7a9bc
Romeo Valentin <romeov@stanford.edu>
2023-05-22 14:21:18 -0700

    Improve constraints, use Gurobi

--- prototype.jl
+++ prototype.jl
@@ -2,31 +2,47 @@ using Revise
 using LinearAlgebra
 using JuMP, DisjunctiveProgramming
 
-using Juniper, Ipopt, HiGHS
+using Juniper, Ipopt, HiGHS, GLPK, Pavito, Gurobi
 ipopt = optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0)
 highs = optimizer_with_attributes(HiGHS.Optimizer, "output_flag" => false)
-optimizer = optimizer_with_attributes(Juniper.Optimizer, "nl_solver"=>ipopt)
+
+# optimizer = optimizer_with_attributes(
+#         Juniper.Optimizer,
+#         "nl_solver" => ipopt,
+#         "mip_solver" => highs,
+#     )
+
+# optimizer =  optimizer_with_attributes(
+#         Pavito.Optimizer,
+#         "mip_solver" => highs,
+#         "cont_solver" => optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0),
+#         "rel_gap" => 1e-4,
+#     )
+
+optimizer = Gurobi.Optimizer
 
 includet("src/data_wranglage.jl")
 
-# vars, data = read_input("data/small.csv")
-vars, data = read_input("data/medium.csv")
+vars, data = read_input("data/small.csv")
+# vars, data = read_input("data/medium.csv")
 data = data' .|> Int
 
 S = 1/size(data, 1) * data'*data
 
 ϵ = 1e-4
 
-m = Model(    optimizer_with_attributes(
-        Juniper.Optimizer,
-        "nl_solver" => ipopt,
-        "mip_solver" => highs,
-    ),)
+m = Model(optimizer)
 @variable(m, 1  <= o[i=1:length(vars)]                   <=length(vars), Int)
 @variable(m, 0. <= x[i=1:length(vars), j=1:length(vars)] <= 1.)
 
+N = length(vars)
+target_idx = N
+@constraint(m, sum(x[N, :]) == 0)
+@constraint(m, [i=1:(N-1)], sum(x[i, :]) >= 1.)
+@constraint(m, [i=1:N], sum(x[i, i]) == 0.)
 
-for i in 1:length(vars), j in 1:length(vars)
+
+for i in 1:N, j in (i+1):N
   add_disjunction!(m,
                @constraints(m, begin
                               x[i, j] >= ϵ
@@ -43,13 +59,18 @@ for i in 1:length(vars), j in 1:length(vars)
                               x[j, i] == 0
                           end),
                reformulation=:hull,
-               # name=Symbol("y$i$j")
+               # M=100.,
+               name=Symbol("y_$(i)_$(j)")
   )
+  choose!(m, 1, m[Symbol("y_$(i)_$(j)")]...; mode = :exactly, name = "XOR") #XOR constraint
 end
 
-@objective(m, Min, 1//2*tr((I - x) * (I - x)' * S))
+λ = 0.1
+@objective(m, Min, 1//2*tr((I - x) * (I - x)' * S) + λ*sum(x))
 
 optimize!(m)
+
+## PROCESS SOLUTION
 
 x_ = let x_ = value.(x)
   x_[x_.<ϵ] .= 0
